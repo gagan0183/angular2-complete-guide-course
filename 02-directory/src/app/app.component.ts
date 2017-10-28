f475aebfc97b83282544a1e38f9f448bc6479e70
gagan0183 <g.deepsingh1@gmail.com>
2017-10-28 15:04:29 +0530

    structurals directive ngswitch

--- 02-directory/src/app/app.component.ts
+++ 02-directory/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   private switch = true;
   private items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  private value = 90;
 
   onSwitch() {
     this.switch = !this.switch;
