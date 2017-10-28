f8c82c8cba41438b330f381d42936a3d809b4edf
gagan0183 <g.deepsingh1@gmail.com>
2017-10-28 14:45:54 +0530

    ngIf directive

--- 02-directory/src/app/app.component.ts
+++ 02-directory/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir works!';
+  private switchField = true;
+
+  switch() {
+    this.switchField = !this.switchField;
+  }
 }
