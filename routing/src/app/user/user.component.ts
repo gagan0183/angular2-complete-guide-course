d3709c5f933c8efc4dcbad1847ee874d2c14b942
gagan0183 <g.deepsingh1@gmail.com>
2017-10-30 22:09:19 +0530

    comp activatedRouters

--- routing/src/app/user/user.component.ts
+++ routing/src/app/user/user.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/rx';
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
-      <a [routerLink]="['/user']">User</a>
       <button (click)="onNavigate()">Go home</button>
+      {{id}}
     `
 })
 export class UserComponent {
+  private subscription: Subscription;
+
+  private id: string;
+
   onNavigate() {
     this.router.navigate(['/']);
   }
-  constructor(private router: Router) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.subscription = this.activatedRoute.params.subscribe(
+      (params) => this.id = params['id']
+    );
+  }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
