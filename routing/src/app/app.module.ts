6af6dd680175a22b706e08e1f84eb1653557baff
gagan0183 <g.deepsingh1@gmail.com>
2017-10-30 20:36:45 +0530

    starting routing project

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home-component.component';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- routing/src/app/home-component.component.ts
+++ routing/src/app/home-component.component.ts
@@ -1,31 +1,16 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Router } from "@angular/router";
-import { Subscription } from "rxjs/Rx";
+import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-home-component',
   template: `
     <h1>
       Home Component!
     </h1>
-    <hr>
-    {{param}}
   `,
   styles: []
 })
-export class HomeComponent implements OnDestroy {
-  private subscription: Subscription;
+export class HomeComponent {
+  constructor() {
 
-  param: string;
-
-  constructor(private router: Router) {
-    this.subscription = router.routerState.queryParams.subscribe(
-      (queryParam: any) => this.param = queryParam['analytics']
-    );
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
