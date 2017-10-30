ef1693f316a061e371753a459de2c893f606768a
gagan0183 <g.deepsingh1@gmail.com>
2017-10-30 23:49:26 +0530

    comp query params

--- routing/src/app/home-component.component.ts
+++ routing/src/app/home-component.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/rx';
 
 @Component({
   selector: 'app-home-component',
@@ -6,11 +8,22 @@ import { Component } from '@angular/core';
     <h1>
       Home Component!
     </h1>
+    <hr>
+    {{param}}
   `,
   styles: []
 })
-export class HomeComponent {
-  constructor() {
+export class HomeComponent implements OnDestroy {
+  param: string;
+  private subscription: Subscription;
 
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.subscription = this.activatedRoute.queryParams.subscribe(
+      (queryParam: any) => this.param = queryParam['analytics']
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
