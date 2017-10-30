2a36f5f24e5c01359a109abdf2a55f30550c8622
gagan0183 <g.deepsingh1@gmail.com>
2017-10-30 20:55:00 +0530

    navigate through code

--- routing/src/app/user/user.component.ts
+++ routing/src/app/user/user.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <a [routerLink]="['/user']">User</a>
+      <button (click)="onNavigate()">Go home</button>
     `
 })
 export class UserComponent {
-
-  constructor() {
+  onNavigate() {
+    this.router.navigate(['/']);
+  }
+  constructor(private router: Router) {
 
   }
 }
