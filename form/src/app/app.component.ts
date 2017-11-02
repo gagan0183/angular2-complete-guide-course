0271ec90a1e0ec4fd6d5af930b86e866ffe3ee7d
gagan0183 <g.deepsingh1@gmail.com>
2017-11-02 20:54:57 +0530

    comp the forms

---
+++ form/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { TemplateDrivenComponent } from "./template-driven/template-driven.component";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'forms.component.html'
+})
+export class AppComponent {
+}
